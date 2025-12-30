d10434efcbbc9d999ce61b2e13c2c02a2b007efd
ujwal sahu <ujwalsahucs10@gmail.com>
2025-12-30 21:56:01 +0530

    new commit after changin git history

--- 1_cliberation/4_final/1.2_Auto_calib.py
+++ 1_cliberation/4_final/1.2_Auto_calib.py
@@ -14,3 +14,67 @@
 # 4) 
 
 # 5) so now what happens is ardu get the latest offsets from py . and stores the latest once, and if not calc the offset then no update in offset.txt file. and if not able to get offset from offset.txt file then also no worries and the arduino uses the hardcoded once.
+
+
+
+
+
+
+
+
+
+
+#  Accel calc Algo ---------------------------------------------------------------------
+
+# def calibrate_accelerometer(data, g=1.0):
+#     """
+#     Robust Magneto-style accelerometer calibration.
+
+#     data : Nx3 numpy array (scaled to g units)
+#     g    : target gravity (1.0 or local g)
+
+#     Returns:
+#         A : 3x3 scale / misalignment matrix
+#         b : 3x1 bias vector
+#     """
+
+#     x = data[:, 0]
+#     y = data[:, 1]
+#     z = data[:, 2]
+
+#     # Design matrix
+#     D = np.column_stack([
+#         x*x, y*y, z*z,
+#         2*x*y, 2*x*z, 2*y*z,
+#         2*x, 2*y, 2*z,
+#         np.ones_like(x)
+#     ])
+
+#     # Solve least squares
+#     _, _, Vt = np.linalg.svd(D, full_matrices=False)
+#     beta = Vt[-1, :]  # smallest singular vector
+
+#     # Quadratic form
+#     Q = np.array([
+#         [beta[0], beta[3], beta[4]],
+#         [beta[3], beta[1], beta[5]],
+#         [beta[4], beta[5], beta[2]]
+#     ])
+
+#     p = beta[6:9]
+#     c = beta[9]
+
+#     # Bias (center of ellipsoid)
+#     b = -np.linalg.inv(Q) @ p
+
+#     # Normalize so surface maps to g
+#     k = (b @ Q @ b - c)
+#     Qn = Q / k * (g * g)
+
+#     # Eigen-decomposition (SAFE)
+#     eigvals, eigvecs = np.linalg.eigh(Qn)
+
+#     # Build correction matrix
+#     A = eigvecs @ np.diag(np.sqrt(eigvals)) @ eigvecs.T
+
+#     return A, b
